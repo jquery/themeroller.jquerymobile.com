d8988649b7ed091e443aeb0f5d710c5194f545a5
Eddie Monge Jr. <eddie@eddiemonge.com>
2011-11-02 13:01:39 -0700

    Minor code cleanup

--- generate_url.php
+++ generate_url.php
@@ -6,18 +6,18 @@
 	$dir = scandir('css/user_themes');
 	$today = date('Ymd', strtotime('today'));
 	$last_file_num = 0;
-	foreach($dir as $file) {
-		if($file != '.' && $file != '..' && $file != '.DS_Store') {
+	foreach ( $dir as $file ) {  	
+    if($file != '.' && $file != '..' && $file != '.DS_Store') {
 			$file_name = explode('.', $file);
-			if(isset($file_name[0])) {
+			if ( isset($file_name[0]) ) {
 				$date = explode('-', $file_name[0]);
 				$file_num = $date[1];
 				$date = $date[0];
 					
-				if(is_numeric($file_num) && $file_num > $last_file_num && $date == $today) {
+				if ( is_numeric($file_num) && $file_num > $last_file_num && $date == $today ) {
 					$last_file_num = $file_num;
 				}
-				if(strtotime('today - 30 days') >= strtotime($date)) {
+				if ( strtotime('today - 30 days') >= strtotime($date) ) {
 					unlink('css/user_themes/' . $file);
 				}
 			}
