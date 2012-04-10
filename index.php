5ad8903acd9048edd586520d2883970271ae3a43
toddparker <todd@filamentgroup.com>
2012-04-10 18:00:33 -0400

    Restored the highlight class

--- index.php
+++ index.php
@@ -122,7 +122,7 @@ if ( isset($_POST['style']) ) {
   &lt;title&gt;jQuery Mobile page&lt;/title&gt;
   &lt;meta charset="utf-8" /&gt;
   &lt;meta name="viewport" content="width=device-width, initial-scale=1"&gt;
-  <span class="highlight">&lt;link rel="stylesheet" href="css/themes/my-custom-theme.css" /&gt;</span>
+  <b class="highlight">&lt;link rel="stylesheet" href="css/themes/my-custom-theme.css" /&gt;</b>
   &lt;link rel="stylesheet" href="http://code.jquery.com/mobile/<?php echo $JQM_VERSION; ?>/jquery.mobile.structure-<?php echo $JQM_VERSION; ?>.min.css" /&gt; 
   &lt;script src="http://code.jquery.com/jquery-<?php echo $JQUERY_VERSION; ?>.min.js"&gt;&lt;/script&gt; 
   &lt;script src="http://code.jquery.com/mobile/<?php echo $JQM_VERSION; ?>/jquery.mobile-<?php echo $JQM_VERSION; ?>.min.js"&gt;&lt;/script&gt; 
