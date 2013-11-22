5f9312f341a11b7790e79e0759f9c58755d3061d
Gabriel Schulhof <gabriel.schulhof@intel.com>
2013-11-22 14:31:37 +0200

    Template: Add explicit theme to several elements to avoid inheritance.

--- js/app.js
+++ js/app.js
@@ -1196,36 +1196,36 @@ TR.initThemeRoller = function() {
 					"</div>" +
 					"<div class=\"ui-content ui-page-theme-a\" data-form=\"ui-page-theme-a\" data-theme=\"a\" role=\"main\">" +
 						"<p>Sample text and <a data-form=\"ui-body-a\" href=\"#\" data-theme=\"a\">links</a>.</p>" +
-						"<ul data-role=\"listview\" data-inset=\"true\">" +
+						"<ul data-role=\"listview\" data-inset=\"true\" data-theme=\"a\">" +
 							"<li data-role=\"list-divider\" data-theme=\"a\" data-swatch=\"a\" data-form=\"ui-bar-a\">List Header</li>" +
 							"<li data-form=\"ui-body-a\" data-swatch=\"a\" data-theme=\"a\">Read-only list item</li>" +
 							"<li><a class=\"ui-btn-a ui-btn\" data-form=\"ui-btn-up-a\" data-swatch=\"a\" data-theme=\"a\" href=\"#\">Linked list item</a></li>" +
 						"</ul>" +
 						"<div data-role=\"fieldcontain\">" +
 							"<fieldset data-role=\"controlgroup\">" +
-								"<input type=\"radio\" name=\"radio-choice-a\" id=\"radio-choice-1-a\" value=\"choice-1\" checked=\"checked\" />" +
+								"<input type=\"radio\" data-theme=\"a\" name=\"radio-choice-a\" id=\"radio-choice-1-a\" value=\"choice-1\" checked=\"checked\" />" +
 								"<label for=\"radio-choice-1-a\" data-form=\"ui-btn-up-a\">Radio</label>" +
-								"<input type=\"checkbox\" name=\"checkbox-a\" id=\"checkbox-a\" checked=\"checked\" />" +
+								"<input type=\"checkbox\" data-theme=\"a\" name=\"checkbox-a\" id=\"checkbox-a\" checked=\"checked\" />" +
 								"<label for=\"checkbox-a\" data-form=\"ui-btn-up-a\">Checkbox</label>" +
 							"</fieldset>" +
 						"</div>" +
 						"<div data-role=\"fieldcontain\">" +
 							"<fieldset data-role=\"controlgroup\" data-type=\"horizontal\">" +
-								"<input type=\"radio\" name=\"radio-view-a\" id=\"radio-view-a-a\" value=\"list\" checked=\"checked\"/>" +
+								"<input type=\"radio\" data-theme=\"a\" name=\"radio-view-a\" id=\"radio-view-a-a\" value=\"list\" checked=\"checked\"/>" +
 								"<label for=\"radio-view-a-a\" data-form=\"ui-btn-up-a\">On</label>" +
-								"<input type=\"radio\" name=\"radio-view-a\" id=\"radio-view-b-a\" value=\"grid\" />" +
+								"<input type=\"radio\" data-theme=\"a\" name=\"radio-view-a\" id=\"radio-view-b-a\" value=\"grid\" />" +
 								"<label for=\"radio-view-b-a\" data-form=\"ui-btn-up-a\">Off</label>" +
 							"</fieldset>" +
 						"</div>" +
 						"<div data-role=\"fieldcontain\">" +
-							"<select name=\"select-choice\" id=\"select-choice-a\" data-native-menu=\"false\" data-theme=\"a\" data-form=\"ui-btn-up-a\">" +
+							"<select name=\"select-choice\" data-theme=\"a\" id=\"select-choice-a\" data-native-menu=\"false\" data-theme=\"a\" data-form=\"ui-btn-up-a\">" +
 								"<option value=\"standard\">Option 1</option>" +
 								"<option value=\"rush\">Option 2</option>" +
 								"<option value=\"express\">Option 3</option>" +
 								"<option value=\"overnight\">Option 4</option>" +
 							"</select>" +
 						"</div>" +
-						"<input type=\"text\" value=\"Text Input\" class=\"input\" data-form=\"ui-body-a\" />" +
+						"<input type=\"text\" value=\"Text Input\" data-theme=\"a\" class=\"input\" data-form=\"ui-body-a\" />" +
 						"<div data-role=\"fieldcontain\">" +
 							"<input type=\"range\" name=\"slider\" value=\"50\" min=\"0\" max=\"100\" data-form=\"ui-body-a\" data-theme=\"a\" data-highlight=\"true\" />" +
 						"</div>" +
