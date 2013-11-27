9cfe798bd83961277c08426bc3ab5b72a0e34633
Jasper de Groot <mail@ugomobi.com>
2013-11-27 21:20:01 +0100

    Include the PNG icons and icon CSS in the ZIP
    And switched to minified icon CSS.

--- zip.php
+++ zip.php
@@ -82,18 +82,35 @@
 	    $match = preg_replace("/url\(/", "", $match);
 	    $match = preg_replace("/\)/", "", $match);
 	}
-	
+	    
 	//add files to zip and echo it back to page
-	$zip->addFromString("themes/images/icons-18-white.png", file_get_contents("http://code.jquery.com/mobile/" . $JQM_VERSION . "/images/icons-18-white.png"));
-	$zip->addFromString("themes/images/icons-18-black.png", file_get_contents("http://code.jquery.com/mobile/" . $JQM_VERSION . "/images/icons-18-black.png"));
-	$zip->addFromString("themes/images/icons-36-white.png", file_get_contents("http://code.jquery.com/mobile/" . $JQM_VERSION . "/images/icons-36-white.png"));
-	$zip->addFromString("themes/images/icons-36-black.png", file_get_contents("http://code.jquery.com/mobile/" . $JQM_VERSION . "/images/icons-36-black.png"));
+	if(preg_match("/1.4/", $JQM_VERSION)) {
+		$JQM_ICONS_LINK = "		<link rel=\"stylesheet\" href=\"themes/jquery.mobile.icons.min.css\" />\n";
+
+		$zip->addFromString("themes/jquery.mobile.icons.min.css", file_get_contents("jqm/" . $JQM_VERSION . "/jquery.mobile.icons.min.css"));
+
+	    $zip->addEmptyDir("themes/images/icons-png/");
+
+        foreach(glob("jqm/" . $JQM_VERSION . "/images/icons-png/*") as $file) {
+        	$name = str_replace("jqm/" . $JQM_VERSION . "/images/icons-png/", "", $file);
+        	$zip->addFile($file, "themes/images/icons-png/" . $name);
+        }
+
+
+	} else {
+		$JQM_ICONS_LINK = "";
+
+		$zip->addFromString("themes/images/icons-18-white.png", file_get_contents("http://code.jquery.com/mobile/" . $JQM_VERSION . "/images/icons-18-white.png"));
+		$zip->addFromString("themes/images/icons-18-black.png", file_get_contents("http://code.jquery.com/mobile/" . $JQM_VERSION . "/images/icons-18-black.png"));
+		$zip->addFromString("themes/images/icons-36-white.png", file_get_contents("http://code.jquery.com/mobile/" . $JQM_VERSION . "/images/icons-36-white.png"));
+		$zip->addFromString("themes/images/icons-36-black.png", file_get_contents("http://code.jquery.com/mobile/" . $JQM_VERSION . "/images/icons-36-black.png"));
+	}
 	$zip->addFromString("themes/" . $match, file_get_contents("http://code.jquery.com/mobile/" . $JQM_VERSION . "/" . $match));
 	$zip->addFromString("themes/" . $theme_name . ".css", $uncompressed);
 	$zip->addFromString("themes/" . $theme_name . ".min.css", $compressed);
 	//$zip->addFromString("js/jquery.mobile.min.js", htmlspecialchars(file_get_contents("http://code.jquery.com/mobile/latest/jquery.mobile.min.js")));
 	//$zip->addFromString("js/jquery.min.js", htmlspecialchars(file_get_contents("http://code.jquery.com/jquery.min.js")));
-	$zip->addFromString("index.html", "\n<!DOCTYPE html>\n<html>\n	<head>\n		<meta charset=\"utf-8\">\n		<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n		<title>jQuery Mobile: Theme Download</title>\n		<link rel=\"stylesheet\" href=\"themes/" . $theme_name . ".min.css\" />\n		<link rel=\"stylesheet\" href=\"http://code.jquery.com/mobile/" . $JQM_VERSION . "/jquery.mobile.structure-" . $JQM_VERSION . ".min.css\" />\n		<script src=\"http://code.jquery.com/jquery-" . $JQUERY_VERSION . ".min.js\"></script>\n		<script src=\"http://code.jquery.com/mobile/" . $JQM_VERSION . "/jquery.mobile-" . $JQM_VERSION . ".min.js\"></script>\n	</head>\n	<body>\n		<div data-role=\"page\" data-theme=\"a\">\n			<div data-role=\"header\" data-position=\"inline\">\n				<h1>It Worked!</h1>\n			</div>\n			<div data-role=\"content\" data-theme=\"a\">\n				<p>Your theme was successfully downloaded. You can use this page as a reference for how to link it up!</p>\n				<pre>\n<strong>&lt;link rel=&quot;stylesheet&quot; href=&quot;themes/" . $theme_name . ".min.css&quot; /&gt;</strong>\n&lt;link rel=&quot;stylesheet&quot; href=&quot;http://code.jquery.com/mobile/" . $JQM_VERSION . "/jquery.mobile.structure-" . $JQM_VERSION . ".min.css&quot; /&gt;\n&lt;script src=&quot;http://code.jquery.com/jquery-" . $JQUERY_VERSION . ".min.js&quot;&gt;&lt;/script&gt;\n&lt;script src=&quot;http://code.jquery.com/mobile/" . $JQM_VERSION . "/jquery.mobile-" . $JQM_VERSION . ".min.js&quot;&gt;&lt;/script&gt;\n				</pre>\n				<p>This is content color swatch \"A\" and a preview of a <a href=\"#\" class=\"ui-link\">link</a>.</p>\n				<label for=\"slider1\">Input slider:</label>\n				<input type=\"range\" name=\"slider1\" id=\"slider1\" value=\"50\" min=\"0\" max=\"100\" data-theme=\"a\" />\n				<fieldset data-role=\"controlgroup\"  data-type=\"horizontal\" data-role=\"fieldcontain\">\n				<legend>Cache settings:</legend>\n				<input type=\"radio\" name=\"radio-choice-a1\" id=\"radio-choice-a1\" value=\"on\" checked=\"checked\" />\n				<label for=\"radio-choice-a1\">On</label>\n				<input type=\"radio\" name=\"radio-choice-a1\" id=\"radio-choice-b1\" value=\"off\"  />\n				<label for=\"radio-choice-b1\">Off</label>\n				</fieldset>\n			</div>\n		</div>\n	</body>\n</html>");
+	$zip->addFromString("index.html", "\n<!DOCTYPE html>\n<html>\n	<head>\n		<meta charset=\"utf-8\">\n		<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n		<title>jQuery Mobile: Theme Download</title>\n		<link rel=\"stylesheet\" href=\"themes/" . $theme_name . ".min.css\" />\n" . $JQM_ICONS_LINK . "		<link rel=\"stylesheet\" href=\"http://code.jquery.com/mobile/" . $JQM_VERSION . "/jquery.mobile.structure-" . $JQM_VERSION . ".min.css\" />\n		<script src=\"http://code.jquery.com/jquery-" . $JQUERY_VERSION . ".min.js\"></script>\n		<script src=\"http://code.jquery.com/mobile/" . $JQM_VERSION . "/jquery.mobile-" . $JQM_VERSION . ".min.js\"></script>\n	</head>\n	<body>\n		<div data-role=\"page\" data-theme=\"a\">\n			<div data-role=\"header\" data-position=\"inline\">\n				<h1>It Worked!</h1>\n			</div>\n			<div data-role=\"content\" data-theme=\"a\">\n				<p>Your theme was successfully downloaded. You can use this page as a reference for how to link it up!</p>\n				<pre>\n<strong>&lt;link rel=&quot;stylesheet&quot; href=&quot;themes/" . $theme_name . ".min.css&quot; /&gt;</strong>\n&lt;link rel=&quot;stylesheet&quot; href=&quot;http://code.jquery.com/mobile/" . $JQM_VERSION . "/jquery.mobile.structure-" . $JQM_VERSION . ".min.css&quot; /&gt;\n&lt;script src=&quot;http://code.jquery.com/jquery-" . $JQUERY_VERSION . ".min.js&quot;&gt;&lt;/script&gt;\n&lt;script src=&quot;http://code.jquery.com/mobile/" . $JQM_VERSION . "/jquery.mobile-" . $JQM_VERSION . ".min.js&quot;&gt;&lt;/script&gt;\n				</pre>\n				<p>This is content color swatch \"A\" and a preview of a <a href=\"#\" class=\"ui-link\">link</a>.</p>\n				<label for=\"slider1\">Input slider:</label>\n				<input type=\"range\" name=\"slider1\" id=\"slider1\" value=\"50\" min=\"0\" max=\"100\" data-theme=\"a\" />\n				<fieldset data-role=\"controlgroup\"  data-type=\"horizontal\" data-role=\"fieldcontain\">\n				<legend>Cache settings:</legend>\n				<input type=\"radio\" name=\"radio-choice-a1\" id=\"radio-choice-a1\" value=\"on\" checked=\"checked\" />\n				<label for=\"radio-choice-a1\">On</label>\n				<input type=\"radio\" name=\"radio-choice-a1\" id=\"radio-choice-b1\" value=\"off\"  />\n				<label for=\"radio-choice-b1\">Off</label>\n				</fieldset>\n			</div>\n		</div>\n	</body>\n</html>");
 	$zip->close();
 	echo ($filename);
 	
