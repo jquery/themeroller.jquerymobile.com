ed372855c3c88fe6be073cc9e3bbf5fc5af49122
Jasper de Groot <mail@ugomobi.com>
2014-06-19 21:40:33 +0200

    Version: Get source files from the zip file instead of the CDN
    Fixes gh-161

--- add_version.php
+++ add_version.php
@@ -4,7 +4,8 @@
     if ( isset( $argv ) ) {
         $jqm = isset( $argv[ 1 ] ) ? $argv[ 1 ] : '';
         $jq = isset( $argv[ 2 ] ) ? $argv[ 2 ] : '';
-        $jqm_path ="http://code.jquery.com/mobile/" . $jqm;
+        $jqm_zip ="http://jquerymobile.com/resources/download/jquery.mobile-" . $jqm . ".zip";
+        $jqm_dir = "jquery.mobile-" . $jqm;
         $jq_path ="http://code.jquery.com/jquery-" . $jq . ".min.js";
 
         chdir( 'jqm' );
@@ -21,43 +22,84 @@
         
         mkdir( $jqm );
         chdir( $jqm );
-                
+    
+        //Get the jqm zip file
+        echo "Downloading the jQuery Mobile ZIP file...\n";
+        getFile( $jqm_zip, $jqm_dir . ".zip" );
+
+        //Extract the files to a temporary directory 
+        $zip = new ZipArchive;
+        if ( $zip->open( $jqm_dir . ".zip" ) === true ) {
+            $zip->extractTo( $jqm_dir );
+            $zip->close();
+        } else {
+            echo "Could not extract ZIP file :(";
+        }
+
+        //Delete the zip
+        unlink( $jqm_dir . ".zip" );
+
         //Get jQuery JS
         echo "Getting jQuery JS...\n";
         getFile( $jq_path, "jquery.min.js" );
+
         //Get jQuery Mobile Images
         echo "Getting jQuery Mobile Images...\n";
         mkdir( 'images' );
         chdir( 'images' );
-        getFile( $jqm_path . "/images/ajax-loader.gif", "ajax-loader.gif" );
-        chdir( '..' );
+
+        copy( "../" . $jqm_dir . "/images/ajax-loader.gif", "ajax-loader.gif" );
+
+        mkdir( 'icons-png' );
+        chdir( 'icons-png' );
+
+        if ( $dir = opendir( "../../" . $jqm_dir . "/images/icons-png/" ) ) {
+
+            while ( false !== ( $file = readdir( $dir ) ) ) {
+                if ( ! is_dir( $file ) ) {
+                    copy( "../../" . $jqm_dir . "/images/icons-png/" . $file, $file );
+                }
+            }
+
+            closedir( $dir );
+        }
+
+        chdir( '../..' );
+
         //Get jQuery Mobile JS
         echo "Getting jQuery Mobile JS...\n";
-        getFile( $jqm_path . "/jquery.mobile-" . $jqm . ".min.js", "jqm.min.js" );
+        copy( $jqm_dir . "/jquery.mobile-" . $jqm . ".min.js", "jqm.min.js" );
         //Get jQuery Mobile structure
         echo "Getting jQuery Mobile structure CSS...\n";
-        getFile( $jqm_path . "/jquery.mobile.structure-" . $jqm . ".css", "jqm.structure.css" );
+        copy( $jqm_dir . "/jquery.mobile.structure-" . $jqm . ".css", "jqm.structure.css" );
         //Get jQuery Mobile default
         echo "Getting jQuery Mobile default theme CSS...\n";
-        getFile( $jqm_path . "/jquery.mobile.theme-" . $jqm . ".css", "jqm.default.theme.css" );
+        copy( $jqm_dir . "/jquery.mobile.theme-" . $jqm . ".css", "jqm.default.theme.css" );
+        //Get jQuery Mobile icons CSS
+        echo "Getting jQuery Mobile icons CSS...\n";
+        copy( $jqm_dir . "/jquery.mobile.icons-" . $jqm . ".min.css", "jquery.mobile.icons.min.css" );
+
+        //Remove the temporary directory
+        rrmdir( $jqm_dir );
+
         //Create TR starter theme
         echo "Generating ThemeRoller starter theme CSS...\n";
         @createStarter( "jqm.default.theme.css", "jqm.starter.theme.css" );
         //Copy preview.html from most recent version present
         echo "Generating Preview Markup...\n";
         $contents = @file_get_contents( '../' . $most_recent . '/preview.html' );
-		writeFile( 'preview.html', $contents );
-		//Create user_themes directory with README placeholder
+        writeFile( 'preview.html', $contents );
+        //Create user_themes directory with README placeholder
         echo "Creating user_themes directory...\n";
         mkdir( "user_themes" );
-		writeFile( 'user_themes/README.md', 'This is where theme files are temporarily stored when a user "shares" a theme.' );
-		//Create empty panel.js file
-		writeFile( 'panel.js', '' );
-		//Altering version.php with new key value pair for jQm, and jQuery versions
-		chdir( '../..' );
-		$contents = file_get_contents( 'version.php' );
-		$contents = preg_replace( "/(\\\$ALL_JQUERY_VERSIONS.*)\n.?\);/s", "$1,\n\t\"" . $jqm . "\" => \"" . $jq . "\"\n);", $contents, 1 );
-		writeFile( 'version.php', $contents );
+        writeFile( 'user_themes/README.md', 'This is where theme files are temporarily stored when a user "shares" a theme.' );
+        //Create empty panel.js file
+        writeFile( 'panel.js', '' );
+        //Altering version.php with new key value pair for jQm, and jQuery versions
+        chdir( '../..' );
+        $contents = file_get_contents( 'version.php' );
+        $contents = preg_replace( "/(\\\$ALL_JQUERY_VERSIONS.*)\n.?\);/s", "$1,\n\t\"" . $jqm . "\" => \"" . $jq . "\"\n);", $contents, 1 );
+        writeFile( 'version.php', $contents );
         
     } else {
         echo 'This script must be executed via command line';
@@ -70,19 +112,19 @@
     function getFile( $url, $filename ) {
         if ( check404( $url ) ) {
             echo $url . " could not be found. Generating empty file for " . $filename . "\n";
-    		writeFile( $filename, '' );
-    		return false;
-    	} else {
-    	    //get contents
-    		$handle = curl_init( $url );
-    		curl_setopt($handle, CURLOPT_RETURNTRANSFER, 1);
-    		$contents = curl_exec( $handle );
-    		curl_close( $handle );
-		
-    		//write them out to local file
-    		writeFile( $filename, $contents );
-    		return true;
-    	}
+            writeFile( $filename, '' );
+            return false;
+        } else {
+            //get contents
+            $handle = curl_init( $url );
+            curl_setopt($handle, CURLOPT_RETURNTRANSFER, 1);
+            $contents = curl_exec( $handle );
+            curl_close( $handle );
+        
+            //write them out to local file
+            writeFile( $filename, $contents );
+            return true;
+        }
     }
     
     function writeFile( $filename, $string ) {
